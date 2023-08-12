e6c9ad4dabdff1f04471f009986308c3fddc67bf
MΛX <mxcop.dev@gmail.com>
2023-08-13 00:38:18 +0200

    init

---
+++ data.lua
@@ -0,0 +1,11 @@
+data.raw.tool["module-circuit-board"].icon = "__reskins-bobs-modules__/graphics/icons/module-intermediates/module-circuit-board.png"
+data.raw.tool["module-circuit-board"].icon_size = 64
+data.raw.tool["module-case"].icon = "__reskins-bobs-modules__/graphics/icons/module-intermediates/module-case.png"
+data.raw.tool["module-case"].icon_size = 64
+
+data.raw.tool["speed-processor"].icon = "__reskins-bobs-modules__/graphics/icons/module-intermediates/speed-circuit-board.png"
+data.raw.tool["speed-processor"].icon_size = 64
+data.raw.item["speed-processor-2"].icon = "__reskins-bobs-modules__/graphics/icons/module-intermediates/speed-logic-board.png"
+data.raw.item["speed-processor-2"].icon_size = 64
+data.raw.item["speed-processor-3"].icon = "__reskins-bobs-modules__/graphics/icons/module-intermediates/speed-processing-board.png"
+data.raw.item["speed-processor-3"].icon_size = 64
