28cdd003f3bc0ca8c352465f8e6c600b83750f5a
MΛX <mxcop.dev@gmail.com>
2023-08-13 01:01:02 +0200

    feat: effect & product logic boards

--- data.lua
+++ data.lua
@@ -9,3 +9,9 @@ data.raw.item["speed-processor-2"].icon = "__reskins-bobs-modules__/graphics/ico
 data.raw.item["speed-processor-2"].icon_size = 64
 data.raw.item["speed-processor-3"].icon = "__reskins-bobs-modules__/graphics/icons/module-intermediates/speed-processing-board.png"
 data.raw.item["speed-processor-3"].icon_size = 64
+
+data.raw.item["effectivity-processor-2"].icon = "__reskins-bobs-modules__/graphics/icons/module-intermediates/effectivity-logic-board.png"
+data.raw.item["effectivity-processor-2"].icon_size = 64
+
+data.raw.item["productivity-processor-2"].icon = "__reskins-bobs-modules__/graphics/icons/module-intermediates/productivity-logic-board.png"
+data.raw.item["productivity-processor-2"].icon_size = 64
