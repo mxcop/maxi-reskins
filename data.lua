a976a1d14b227b20113fbe9d2ca142b8a0f9696f
MΛX <mxcop.dev@gmail.com>
2023-08-13 14:32:48 +0200

    feat: module processing boards

--- data.lua
+++ data.lua
@@ -2,11 +2,15 @@ data.raw.tool["module-circuit-board"].icon = "__reskins-bobs-modules__/graphics/
 data.raw.tool["module-circuit-board"].icon_size = 64
 data.raw.tool["module-case"].icon = "__reskins-bobs-modules__/graphics/icons/module-intermediates/module-case.png"
 data.raw.tool["module-case"].icon_size = 64
+data.raw.item["module-contact"].icon = "__reskins-bobs-modules__/graphics/icons/module-intermediates/module-contact.png"
+data.raw.item["module-contact"].icon_size = 64
 
 data.raw.item["module-processor-board"].icon = "__reskins-bobs-modules__/graphics/icons/module-intermediates/basic-module-board.png"
 data.raw.item["module-processor-board"].icon_size = 64
 data.raw.item["module-processor-board-2"].icon = "__reskins-bobs-modules__/graphics/icons/module-intermediates/module-logic-board.png"
 data.raw.item["module-processor-board-2"].icon_size = 64
+data.raw.item["module-processor-board-3"].icon = "__reskins-bobs-modules__/graphics/icons/module-intermediates/module-processing-board.png"
+data.raw.item["module-processor-board-3"].icon_size = 64
 
 data.raw.tool["speed-processor"].icon = "__reskins-bobs-modules__/graphics/icons/module-intermediates/speed-circuit-board.png"
 data.raw.tool["speed-processor"].icon_size = 64
@@ -19,18 +23,26 @@ data.raw.tool["effectivity-processor"].icon = "__reskins-bobs-modules__/graphics
 data.raw.tool["effectivity-processor"].icon_size = 64
 data.raw.item["effectivity-processor-2"].icon = "__reskins-bobs-modules__/graphics/icons/module-intermediates/effectivity-logic-board.png"
 data.raw.item["effectivity-processor-2"].icon_size = 64
+data.raw.item["effectivity-processor-3"].icon = "__reskins-bobs-modules__/graphics/icons/module-intermediates/effectivity-processing-board.png"
+data.raw.item["effectivity-processor-3"].icon_size = 64
 
 data.raw.tool["productivity-processor"].icon = "__reskins-bobs-modules__/graphics/icons/module-intermediates/productivity-circuit-board.png"
 data.raw.tool["productivity-processor"].icon_size = 64
 data.raw.item["productivity-processor-2"].icon = "__reskins-bobs-modules__/graphics/icons/module-intermediates/productivity-logic-board.png"
 data.raw.item["productivity-processor-2"].icon_size = 64
+data.raw.item["productivity-processor-3"].icon = "__reskins-bobs-modules__/graphics/icons/module-intermediates/productivity-processing-board.png"
+data.raw.item["productivity-processor-3"].icon_size = 64
 
 data.raw.tool["pollution-clean-processor"].icon = "__reskins-bobs-modules__/graphics/icons/module-intermediates/pollution-clean-circuit-board.png"
 data.raw.tool["pollution-clean-processor"].icon_size = 64
 data.raw.item["pollution-clean-processor-2"].icon = "__reskins-bobs-modules__/graphics/icons/module-intermediates/pollution-clean-logic-board.png"
 data.raw.item["pollution-clean-processor-2"].icon_size = 64
+data.raw.item["pollution-clean-processor-3"].icon = "__reskins-bobs-modules__/graphics/icons/module-intermediates/pollution-clean-processing-board.png"
+data.raw.item["pollution-clean-processor-3"].icon_size = 64
 
 data.raw.tool["pollution-create-processor"].icon = "__reskins-bobs-modules__/graphics/icons/module-intermediates/pollution-create-circuit-board.png"
 data.raw.tool["pollution-create-processor"].icon_size = 64
 data.raw.item["pollution-create-processor-2"].icon = "__reskins-bobs-modules__/graphics/icons/module-intermediates/pollution-create-logic-board.png"
 data.raw.item["pollution-create-processor-2"].icon_size = 64
+data.raw.item["pollution-create-processor-3"].icon = "__reskins-bobs-modules__/graphics/icons/module-intermediates/pollution-create-processing-board.png"
+data.raw.item["pollution-create-processor-3"].icon_size = 64
